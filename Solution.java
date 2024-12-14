21cd34d21f49077f46284c2fed5a8a36e4adb94d
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2024-12-14 18:16:15 +0530

    Efficient more logical

--- Solution.java
+++ Solution.java
@@ -1,42 +1,46 @@
-public class Solution {
-    public int solve(String A) {
-        int zeroes=0;
-        int totalOnes=0;
-        int left=0;
-        int right=0;
-        int maxL=0;
-        for (int i = 0; i < A.length(); i++) {
-            if(A.charAt(i)=='1'){
-                totalOnes++;
-            }
-        }
-        if(totalOnes==A.length()){
-            return totalOnes;
-        }
-        for(int i=0;i<A.length();i++){
-            if (A.charAt(i)=='1') {
-                if (zeroes==1) {
-                    right++;
-                    maxL= Math.max(maxL, left+right+((left+right)<totalOnes?1:0));
-                }else{
-                    left++;
-                }
-            }else{
-                zeroes++;
-                if (zeroes==1) {
-                   maxL= Math.max(maxL, left+right+((left+right)<totalOnes?1:0));
-                }else{
-                    zeroes=1;
-                    left=right;
-                    right=0;
+    public class Solution {
+        public int solve(String A) {
+            int zeroes=0;
+            int totalOnes=0;
+            int left=0;
+            int right=0;
+            int maxL=0;
+            for (int i = 0; i < A.length(); i++) {
+                if(A.charAt(i)=='1'){   
+                    totalOnes++;
                 }
             }
+            if(totalOnes==A.length()){
+                return totalOnes;
+            }
+            for(int i=0;i<A.length();i++){
+                if (A.charAt(i)=='1') {
+                    if (zeroes==1) {
+                        right++;
+                        // maxL= Math.max(maxL, left+right+((left+right)<totalOnes?1:0));
+                        if (i==A.length()-1) {
+                        maxL= Math.max(maxL, left+right+((left+right)<totalOnes?1:0));
+                        }
+                    }else{
+                        left++;
+                    }
+                }else{
+                    zeroes++;
+                    if (zeroes==1) {
+                    maxL= Math.max(maxL, left+((left+right)<totalOnes?1:0));
+                    }else{
+                        maxL= Math.max(maxL, left+right+((left+right)<totalOnes?1:0));
+                        zeroes=1;
+                        left=right;
+                        right=0;
+                    }
+                }
+            }
+            return maxL;
         }
-        return maxL;
-    }
-    
-    
-    }
-    
-    
+        
+        
+        }
+        
+        
 
