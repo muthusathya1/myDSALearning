8e20809c8750628cb5d5f120885f11d209ac0e2f
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-01 23:30:46 +0530

    Initial Commit

--- Solution.java
+++ Solution.java
@@ -1,23 +1,63 @@
-import java.util.Arrays;
-
 // Problem Description
 
-// You are given two lowercase strings A and B each of length N. Return 1 if they are anagrams to each other and 0 if not.
+// Given two binary strings A and B. Return their sum (also a binary string).
 
-// Note : Two strings A and B are called anagrams to each other if A can be formed after rearranging the letters of B.
+
+// Problem Constraints
+
+// 1 <= length of A <= 105
+
+// 1 <= length of B <= 105
+
+// A and B are binary strings
+
+
+
+// Input Format
+
+// The two argument A and B are binary strings.
+
+
+
+// Output Format
+
+// Return a binary string denoting the sum of A and B
+
+
+
+// Example Input
+
+// Input 1:
+// A = "100"
+// B = "11"
+// Input 2:
+// A = "110"
+// B = "10"
+
+
+// Example Output
+
+// Output 1:
+// "111"
+// Output 2:
+// "1000"
 
 public class Solution {
-    public int solve(String A, String B) {
-        if (A.length() != B.length()) {
-            return 0;
+
+    public String addBinary(String A, String B) {
+        StringBuilder result = new StringBuilder();
+        int i = A.length() - 1, j = B.length() - 1, carry = 0;
+        
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += A.charAt(i--) - '0';
+            if (j >= 0) sum += B.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
         }
         
-        char[] aArray = A.toCharArray();
-        char[] bArray = B.toCharArray();
+        if (carry != 0) result.append(carry);
         
-        Arrays.sort(aArray);
-        Arrays.sort(bArray);
-        
-        return Arrays.equals(aArray, bArray) ? 1 : 0;
+        return result.reverse().toString();
     }
 }
