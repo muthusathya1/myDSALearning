9bc9538638206b6394a0a1cd34aeddfb92fdc60a
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-01 23:23:38 +0530

    ChangeCharacter Optimized

--- Solution.java
+++ Solution.java
@@ -3,28 +3,29 @@ import java.util.*;
 public class Solution {
     public int solve(String A, int B) {
         // Count the frequency of each character in the string
-        int cnt[] = new int[26];
-        // Store the frequency of each character in an arraylist
-        for(int i = 0; i < A.length(); ++i){
-            ++cnt[A.charAt(i)-'a'];
+        int[] cnt = new int[26];
+        for (char c : A.toCharArray()) {
+            cnt[c - 'a']++;
         }
-        // Sort the arraylist
-        ArrayList<Integer> C = new ArrayList<Integer>();
-        for(int i = 0; i < 26; ++i){
-            if(cnt[i] > 0){
-                C.add(cnt[i]);
+
+        // Store the frequency of each character in a priority queue (min-heap)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int frequency : cnt) {
+            if (frequency > 0) {
+                pq.add(frequency);
             }
         }
-        // Sort the arraylist
-        Collections.sort(C);
-        // Remove the elements from the arraylist until the sum of the elements is less than B
-        for(int i = 0; i < C.size(); ++i){
-            // If the sum of the elements is less than B, return the number of elements removed
-            B -= C.get(i);
-            if(B < 0){
-                return C.size() - i;
-            }
+
+        // Remove the elements from the priority queue until the sum of the elements is less than B
+        while (!pq.isEmpty() && B >= pq.peek()) {
+            B -= pq.poll();
         }
-        return 1;
+        
+        // If the priority queue is empty, return 1
+        if (pq.size()==0) {
+            return 1;
+        }
+        // Return the number of elements left in the priority queue
+        return pq.size();
     }
 }
