44fb765c1f154aa7dc49ac5a05c4d0777a09e5fa
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-02 21:40:34 +0530

    Optimized

--- Solution.java
+++ Solution.java
@@ -1,21 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
     public int solve(ArrayList<Integer> A) {
-        HashMap<Long,Integer> sumFreqMap= new HashMap<>();
-        // if(A.size()==1 && A.get(0)==0) return 1;
-        long sum=0;
-        long count=0;
-        long MOD=1000000007;
-        for (int i = 0; i < A.size(); i++) {
-            sum+=A.get(i);
-            if (sum==0)count++;
-            if (sumFreqMap.containsKey(sum)) {
-                count+=sumFreqMap.get(sum);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+
+        for (int num : A) {
+            if (freq.containsKey(num)) {
+                if (freq.get(num) == 1) {
+                    count--;
+                }
+                freq.put(num, freq.get(num) + 1);
+            } else {
+                freq.put(num, 1);
+                count++;
             }
-            sumFreqMap.put(sum, sumFreqMap.getOrDefault(sum, 0)+1);
         }
-        return (int) (count%MOD);
+
+        return count;
     }
 }
+                        
