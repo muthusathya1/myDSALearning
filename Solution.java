57a8acea2e954364bc0b3aeb7aeff25d3174403d
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-02 21:24:24 +0530

    Optimized

--- Solution.java
+++ Solution.java
@@ -1,22 +1,21 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Solution {
-    // Do not write code to include libraries, main() function or accept any input from the console.
-    // Initialization code is already written and hidden from you. Do not write code for it again.
     public int solve(ArrayList<Integer> A) {
-        // Just write your code below to complete the function. Required input is available to you as the function arguments.
-        // Do not print the result or any output. Just return the result via this function.
-        HashSet<Integer> sums=new HashSet<>();
-        int sum=0;
+        HashMap<Long,Integer> sumFreqMap= new HashMap<>();
+        // if(A.size()==1 && A.get(0)==0) return 1;
+        long sum=0;
+        long count=0;
+        long MOD=1000000007;
         for (int i = 0; i < A.size(); i++) {
             sum+=A.get(i);
-            if (sum==0 || A.get(i)==0 || sums.contains(sum)) {
-                return 1;
+            if (sum==0)count++;
+            if (sumFreqMap.containsKey(sum)) {
+                count+=sumFreqMap.get(sum);
             }
-            sums.add(sum);
+            sumFreqMap.put(sum, sumFreqMap.getOrDefault(sum, 0)+1);
         }
-        return 0;
+        return (int) (count%MOD);
     }
-  }
-  
+}
