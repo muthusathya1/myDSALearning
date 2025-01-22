692b2782f74f53de44bcb7cb89392d84e6208045
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-22 23:24:43 +0530

    Initial Commit

--- Solution.java
+++ Solution.java
@@ -1,12 +1,22 @@
 
 public class Solution {
-    public int solve(int A) {
-        if (A==0) {
-            return 0;
-        }
-        return A%10+solve(A/10);
+    static int bar(int x, int y){
+        if(y==0) return 0;
+        return (x + bar(x,y-1));
+    }
+    static int foo(int x, int y){
+        if(y==0) return 1;
+
+        return bar(x, foo(x, y-1));
     }
     public static void main(String[] args) {
-        System.out.println(new Solution().solve(1234));
+        System.out.println(foo(3, 5));
     }
 }
+
+// bar(x,y)= x+ bar(x,y-1)= 2x+ bar(x,y-2)= 3x+ bar(x,y-3)= yx
+
+// foo(x,y) = x* foo(y-1)= x*x*foo(y-2)=.... =x^y 
+
+// 3 ^ 5 = 243
+
