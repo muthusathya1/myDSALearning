cf62495a91966a012ad4f08619d723edc19fd824
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-01-22 23:53:57 +0530

    Initial Commit

--- Solution.java
+++ Solution.java
@@ -1,25 +1,16 @@
 
+// Method 2:
 public class Solution {
-    static int bar(int x, int y){
-        if(y==0) return 0;
-        return (x + bar(x,y-1));
-    }
-    static int foo(int x, int y){
-        if(y==0) return 1;
-
-        return bar(x, foo(x, y-1));
-    }
-    public static void main(String[] args) {
-        System.out.println(foo(3, 5));
+    public int pow(int A, int B, int C) {
+        if(A == 0)
+            return 0;
+        if(B == 0)
+            return 1;
+        long ans = pow(A, B / 2, C);
+        ans = (ans * ans) % C;
+        if(B % 2 == 1)
+            ans = (ans * A);
+        ans = (ans + C) % C;
+        return (int)ans;
     }
 }
-
-// bar(x,y)= x+ bar(x,y-1)= 2x+ bar(x,y-2)= 3x+ bar(x,y-3)= yx
-
-// foo(x,y) = x* foo(y-1)= x*x*foo(y-2)=.... =x^y 
-
-// 3 ^ 5 = 243
-
-
-
-// fun(2,10)= fun(4,5)=4*fun(16,2)=4*fun(256,1)=4*256*1;
