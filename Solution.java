506fd5fc47b57c1bfe97eb6f8f19c12e635a5d45
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-02-23 12:27:36 +0530

    Initial Commit

---
+++ Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int solve(int[] A, int B) {
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > B) {
+                count++;
+            }
+        }
+
+        int swaps = 0;
+        for (int i = 0; i < count; i++) {
+            if (A[i] > B) {
+                swaps++;
+            }
+        }
+        int minSwaps = swaps;
+        int drop = 0;
+        for (int i = count; i < A.length; i++) {
+            if (A[drop] > B) {
+                swaps--;
+            }
+            if (A[i] > B) {
+                swaps++;
+            }
+            drop++;
+            minSwaps = Math.min(swaps, minSwaps);
+        }
+        return minSwaps;
+    }
+}
