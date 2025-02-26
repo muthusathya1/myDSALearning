eaaaa618514235504e0fd7418da27482b10aacb7
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-02-27 00:00:08 +0530

    Commit

--- Solution.java
+++ Solution.java
@@ -1,23 +1,20 @@
-import java.util.ArrayList;
-
 public class Solution {
     public int firstMissingPositive(ArrayList<Integer> A) {
         for (int i = 0; i < A.size(); i++) {
-            if (A.get(i)>0 && A.get(i)!=i+1 && A.get(i)-1<A.size()) {
-                int temp=A.get(A.get(i)-1);
-                A.set(A.get(i)-1,A.get(i));
+            if (A.get(i) > 0 && A.get(i) != i + 1 && A.get(i) - 1 < A.size()) {
+                int temp = A.get(A.get(i) - 1);
+                A.set(A.get(i) - 1, A.get(i));
                 A.set(i, temp);
-                if (A.get(i)>0 && A.get(i)-1<A.size()) {
+                if (A.get(i) > 0 && A.get(i) != i + 1 && A.get(i) - 1 < A.size() && A.get(i) != A.get(A.get(i) - 1)) {
                     i--;
                 }
             }
         }
-
         for (int i = 0; i < A.size(); i++) {
-            if (A.get(i)!=i+1) {
-                return i+1;
+            if (A.get(i) != i + 1) {
+                return i + 1;
             }
         }
-        return A.size()+1;
+        return A.size() + 1;
     }
 }
