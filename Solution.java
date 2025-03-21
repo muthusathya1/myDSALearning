bd2c73559c34f3b4f4f21a5b4d287ebb5e9105bc
MuTHu KRiShNaN P <muthu.krishnan.p@oracle.com>
2025-03-21 22:29:40 +0530

    Commit

--- Solution.java
+++ Solution.java
@@ -1,33 +1,18 @@
-import java.util.Arrays;
-
 public class Solution {
-
-    public static void main(String[] args) {
-        bubbleSort(new int[] { 7, 3, 9, 12, 11 });
-
+    public int solve(int A, int B) {
+        return powerOf(A, B - 2, B);
     }
 
-    public static void bubbleSort(int[] array) {
-        int loops = 0;
-        for (int i = 0; i < array.length; i++) {
-
-            boolean swapped = false;
-            System.out.println();
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
-            }
-            loops++;
-            System.out.println(loops);
+    private int powerOf(int A, int power, int M) {
+        if (power==0) {
+            return 1;
         }
-        System.out.println(Arrays.toString(array));
+        long P=powerOf(A, power/2, M)%M;
+        long X=(P*P)%M;
+        if (power%2==0) {
+            return (int) (X%M);            
+        }else{
+            return (int) (((A%M)*(X%M))%M);
+        }
     }
-
 }
--- SolutionTest.java
+++ SolutionTest.java
@@ -1,13 +1,7 @@
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
+
 public class SolutionTest {
 
-    @Test
-    public void testBubbleSort() {
-       Solution solution=new Solution();
-       int[] array=new int[] { 7, 3, 9, 12, 11 };
-        Solution.bubbleSort(array);
-        assertArrayEquals(array, new int[]{3, 7, 9, 11, 12});
-    }
 }
